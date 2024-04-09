81d700063edcec58e71d968e514ab37f221e90f4
liem nguyen <elminguyen@gmail.com>
2024-04-10 00:23:52 +0300

    insert data

--- database/insertData.php
+++ database/insertData.php
@@ -8,7 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $filename = $_FILES['file']['name'];
     $filetype = $_FILES['file']['type'];
     $filesize = $_FILES['file']['size'];
-    $temp_file = $_FILES['file']['tmp_file'];
+    $temp_file = $_FILES['file']['tmp_name'];
+    $destination = __DIR__ . '/uploads/' . $filename;
+    if (!move_uploaded_file($temp_file, $destination)) {
+      // header('Location: home.php?success=File upload failed');
+      exit;
+    }
 
     $data = [
       'user_id' => 1,
